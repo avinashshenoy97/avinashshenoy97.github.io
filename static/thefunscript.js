c8126ec86a6865bfdb47f604da1320d4be272d9e
avinashshenoy97 <avi123shenoy@hotmail.com>
2017-05-04 11:29:49 +0530

    Animation and styling change

--- static/thefunscript.js
+++ static/thefunscript.js
@@ -1,12 +1,8 @@
 //alert(font);
 document.body.style = "font-family: " + font;
-var lmao = document.createElement("p");
-lmao.innerHTML = "HI";
-document.body.appendChild(lmao);
-
-setTimeout(init, 2000);
-var c = new Array();
-var cleanme = new Array();
+var p = document.createElement("p");
+p.innerHTML = "HI";
+document.body.appendChild(p);
 
 function dark() {
 	var can = document.getElementById("nam");
@@ -49,133 +45,51 @@ function init() {
 	can.id = "can";
 	can.height = window.innerHeight - 100;
 	can.width = window.innerWidth - 100;
-	can.style = "margin: 50px;"
-	can.addEventListener("mousemove", handler, false);
+	can.addEventListener("mousemove", here, false);
 	document.body.appendChild(can);
 	var ctx = can.getContext("2d");
 
 	ctx.fillStyle = "white";
 	ctx.strokeStyle = "white";
 	ctx.fillRect(0, 0, can.width, can.height);
+	rem();
 }
 
-var curx = 0, cury = 0;
-var r = 5;
-
-function handler(event) {
-	here(event.clientX, event.clientY);
-}
-
-function here(x, y) {
-	var tx = x - 50;
-	var ty = y - 50;
-
-	if (tx != curx && ty != cury) {
-		redrawStart();
-		curx = tx;
-		cury = ty;
-		c.push({ x: curx, y: cury });
-		r = 5;
-		var handle = setInterval(move, 150);
-
-		function move() {
-			if (r < 50) {
-				var can = document.getElementById("can");
-				var ctx = can.getContext("2d");
-				ctx.fillStyle = "black";
-				ctx.strokeStyle = "black";
-
-				ctx.beginPath();
-				ctx.arc(curx, cury, r, 0, 2 * Math.PI);
-				ctx.stroke();
-				ctx.fill();
-
-				r += 10;
-			}
-			else {
-				clearInterval(handle);
-			}
-		}
-	}
-}
-
-function redrawStart() {
-	while (c.length > 5) {
-		var startx = c[0].x, starty = c[0].y;
-		var r = 50;
-		var handle = setInterval(redraw, 50);
-		cleanme.push(c.shift());
-	}
-
-	function redraw() {
-		var tx = startx, ty = starty;
-
-		if (Math.abs(curx - tx) < 50 && Math.abs(cury - ty) < 50) {
-			r = 0;
-			//c.unshift({x : startx, y : starty});
-		}
-
-		if (r > 0) {
-			var can = document.getElementById("can");
-			var ctx = can.getContext("2d");
-			ctx.strokeStyle = "white";
-			ctx.lineWidth = 5;
-			ctx.arc(tx, ty, r, 0, 2 * Math.PI);
-			ctx.stroke();
-			r -= 5;
-		}
-		else {
-			clearInterval(handle);
-		}
-	}
-
-	function redrawlast(x, y) {
-		var tx = x - 50;
-		var ty = y - 50;
-
-		var handle = setInterval(move, 50);
-
-		function move() {
-			if (r < 50) {
-				var can = document.getElementById("can");
-				var ctx = can.getContext("2d");
-				ctx.fillStyle = "black";
-				ctx.strokeStyle = "black";
-				ctx.beginPath();
-				ctx.arc(tx, ty, r, 0, 2 * Math.PI);
-				ctx.stroke();
-				ctx.fill();
-
-				r += 10;
-			}
-			else {
-				clearInterval(handle);
-			}
-		}
-	}
-}
-
-function clean() {
-	var c = cleanme;
-	while (c.length > 1) {
-		var can = document.getElementById("can");
-		var ctx = can.getContext("2d");
-		ctx.strokeStyle = "white";
+function here(event) {
+	var canvas = document.getElementById("can");
+	var ctx = canvas.getContext("2d");
+	
+	if (curx != null && cury != null) {
 		ctx.fillStyle = "white";
 		ctx.beginPath();
-		ctx.arc(c[0].x, c[0].y, 50, 0, 2 * Math.PI);
+		ctx.arc(curx, cury, 60, 0, 2 * Math.PI);
+		ctx.fill();
+
+		ctx.fillStyle = "black";
+		ctx.beginPath();
+		ctx.arc(event.clientX - 50, event.clientY - 50, 50, 0, 2 * Math.PI);
 		ctx.stroke();
 		ctx.fill();
-		c.shift();
-	}
 
-	var can = document.getElementById("can");
-	var ctx = can.getContext("2d");
-	ctx.strokeStyle = "black";
-	ctx.fillStyle = "black";
-	ctx.beginPath();
-	ctx.arc(curx, cury, 50, 0, 2 * Math.PI);
-	ctx.stroke();
-	ctx.fill();
+		curx = event.clientX - 50;
+		cury = event.clientY - 50;
+	}
+	else {
+		curx = event.clientX - 50;
+		cury = event.clientY - 50;
+	}
 }
-setInterval(clean, 2000);
+
+function rem() {
+	var canvas = document.getElementById("can");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = "white";
+	ctx.beginPath();
+	ctx.arc(curx, cury, 60, 0, 2 * Math.PI);
+	ctx.fill();
+	curx = cury = null;
+}
+
+curx = null;
+cury = null;
+init();
