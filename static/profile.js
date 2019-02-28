751aa4f2a2da49afa6954bd3c98280c949d6eeff
Avinash Shenoy <avi123shenoy@hotmail.com>
2019-02-28 09:21:37 +0530

    Added horizontal scroll for projects section

--- static/profile.js
+++ static/profile.js
@@ -4,7 +4,7 @@ var header = document.getElementById("header");
 var navBarEle = document.getElementById("navBar");
 
 function checkNavPos() {
-    //console.log(window.scrollY);
+    // console.log(window.scrollY);
     if(window.scrollY >= header.offsetHeight) {
         navBarEle.style.position = "fixed";
         navBarEle.style.top = 0;
@@ -26,3 +26,24 @@ function restartSite() {
     typed = 0;
     typeIt();
 }
+
+offsetStep = window.innerWidth * 4 / 100;
+document.getElementById('projects').addEventListener("wheel", function(event) {
+    if(event.deltaY > 0) {
+        if(($('#projects')[0].offsetLeft - offsetStep) > (-5000 + window.innerWidth)) {
+            $('#projects')[0].style.left = ($('#projects')[0].offsetLeft) - offsetStep + 'px';
+        }
+        else {
+            $('#projects')[0].style.left = (-5000 + window.innerWidth) + 'px';
+        }
+    }
+    else if(event.deltaY < 0) {
+        if(($('#projects')[0].offsetLeft + offsetStep) < 0) {
+            $('#projects')[0].style.left = ($('#projects')[0].offsetLeft) + offsetStep + 'px';
+        }
+        else {
+            $('#projects')[0].style.left = '0px';
+        }
+    }
+    event.preventDefault();
+})
